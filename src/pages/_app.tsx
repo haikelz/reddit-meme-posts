b9e8934f64d7d53e0c6409152c52eda96a682a2b
haikelz <haikel.2017th@gmail.com>
2022-07-28 17:13:36 +0700

    feat: add loading progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import Layout from "../components/layout";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const newClient: QueryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={newClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 };
